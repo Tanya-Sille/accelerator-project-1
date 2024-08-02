fbdbc16fc21d30660644761ddf5972de3dbab5e4
tanya.sille <tanya.sille@gmail.com>
2024-08-02 18:14:26 +0300

    стилизует блоки hero и about, добавляет загрузку видео

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,9 @@
 import {contactListOrder} from './modules/contact-list.js';
 import {initTabs} from './modules/tabs-accordion.js';
+import {initVideo} from './modules/init-video.js';
 import './modules/slider-jury.js';
 import './modules/slider-review.js';
 
 contactListOrder();
 initTabs();
+initVideo();
